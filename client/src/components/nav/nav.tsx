de992221924082237450392617448e36b7d0f4d1
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2025-04-11 10:53:11 -0300

    login em andamento

--- client/src/components/nav/nav.tsx
+++ client/src/components/nav/nav.tsx
@@ -1,17 +1,32 @@
 import './nav.scss';
 import logo from '../../assets/react.svg'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Nav(){
+    const [isHideNav, setIsHideNav] = useState(false);
+    const toggleNav = () => {
+        setIsHideNav(!isHideNav);
+        const navHeight = isHideNav ? '12.5vh' : '0';
+        document.documentElement.style.setProperty('--navHeight', navHeight);
+    }
     return (
-        <nav>
+        <>
+        <nav className={isHideNav ? 'hidden' : ''}>
             <img src={logo} alt="Logo" />
-            <button>^</button>
             <div id="navLinks">
                 <Link id="navLogIn" to='/signin'>Entrar</Link>
                 <Link id="navSignUp" to='/signup'>Cadastrar</Link>
             </div>
         </nav>
+        <div id="navButton">
+            {isHideNav ? (
+                <button onClick={toggleNav}>↓</button>
+            ):(
+                <button onClick={toggleNav}>↑</button>
+            )}
+        </div>
+        </>
     )
 }
 export default Nav;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import MainRoutes from './router';
 import { BrowserRouter } from 'react-router';
-import './global.scss';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
